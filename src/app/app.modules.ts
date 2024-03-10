7c2810b707ef88d9e4e9109f6bfee3f724ae28e5
FrunzaDan <frunzadan96@gmail.com>
2024-03-10 23:11:57 +0200

    initial commit

---
+++ src/app/app.modules.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routes';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [],
+  imports: [AppRoutingModule, NavbarComponent, HomeComponent, FooterComponent],
+  providers: [],
+})
+export class AppModule {}

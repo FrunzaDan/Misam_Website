ffab6316cb65de508a67d3b16df4f303af565ef0
FrunzaDan <frunzadan96@gmail.com>
2024-03-13 18:52:08 +0200

    restructuring

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,27 +1,51 @@
-import { Component, OnInit, importProvidersFrom } from '@angular/core';
-import { ProductsListComponent } from '../products-list/products-list.component';
-import { CartService } from '../../services/cart.service';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { FetchProductsService } from '../../services/fetch-products.service';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { FilterPipe } from '../../shared/filter.pipe';
 
 @Component({
   standalone: true,
-  imports: [ProductsListComponent, RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule, FilterPipe],
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
+  public productList: any = [];
+  filteredProductList = [];
+  searchKey: string = '';
   public totalItem: number = 0;
   public searchTerm!: string;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private api: FetchProductsService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
+    this.api.getProduct().subscribe((res) => {
+      this.productList = res;
+      this.productList.forEach((a: any) => {
+        Object.assign(a, { quantity: 1, total: a.price });
+      });
+    });
+
+    this.cartService.searchString.subscribe((val: any) => {
+      this.searchKey = val;
+    });
+
     this.cartService.getProducts().subscribe((res) => {
       this.totalItem = res.length;
     });
   }
+
+  addtocart(item: any) {
+    this.cartService.addtoCart(item);
+  }
+
   search(event: any) {
     this.searchTerm = (event.target as HTMLInputElement).value;
     console.log(this.searchTerm);
